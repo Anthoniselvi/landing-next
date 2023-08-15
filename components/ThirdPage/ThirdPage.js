a0f4f9c49c0693319bed071cd98d54ec70f84da3
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-15 13:26:45 +0530

    thirdpage corrected

--- components/ThirdPage/ThirdPage.js
+++ components/ThirdPage/ThirdPage.js
@@ -4,26 +4,33 @@ import Image from "next/image";
 const ThirdPage = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.heading}>What we offer</h2>
+      <h2 className={styles.heading}>WHAT WE OFFER</h2>
       <div className={styles.innerbox}>
         <div className={styles.singlebox1}>
+          <div className={styles.topbox1}></div>
           <div className={styles.bottombox1}>
-            <p>Effortless Data Analysis</p>
-            <p>Say goodbye to hours of manual data crunching.</p>
+            <h6 className={styles.toptext}>Effortless Data Analysis</h6>
+            <p className={styles.bottomtext}>
+              Say goodbye to hours of manual data crunching.
+            </p>
           </div>
         </div>
 
         <div className={styles.singlebox2}>
+          <div className={styles.topbox2}></div>
           <div className={styles.bottombox2}>
-            <p>Customer Insights</p>
-            <p>Understand your customer base like never before.</p>
+            <h6 className={styles.toptext}>Customer Insights</h6>
+            <p className={styles.bottomtext}>
+              Understand your customer base like never before.
+            </p>
           </div>
         </div>
 
         <div className={styles.singlebox3}>
+          <div className={styles.topbox3}></div>
           <div className={styles.bottombox3}>
-            <p>User-Friendly Interface</p>
-            <p>
+            <h6 className={styles.toptext}>User-Friendly Interface</h6>
+            <p className={styles.bottomtext}>
               No technical expertise needed. Access valuable insights with ease{" "}
             </p>
           </div>
