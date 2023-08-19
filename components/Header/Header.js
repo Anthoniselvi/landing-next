a85cb6ae04f4339eb7dc15f524aac2247f10f349
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-19 14:29:43 +0530

    upload button corrected

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,18 +1,21 @@
 import styles from "../../styles/Header.module.css";
 import { read, utils } from "xlsx";
 import { useRouter } from "next/router";
-import { useState, useRef } from "react"; // Import 'useRef'
+import { useState, useRef } from "react";
 import { useOrdersContext } from "@/ordersContext";
 
 const Header = () => {
   const router = useRouter();
   const { setOrdersData } = useOrdersContext();
-  const fileInputRef = useRef(null); // Create a ref for the file input element
+  const fileInputRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false); // Add a loading state
 
   const handleFileUpload = async () => {
     if (fileInputRef.current.files.length === 0) {
-      return; // Don't proceed if no file selected
+      return;
     }
+
+    setIsLoading(true); // Start loading animation
 
     const file = fileInputRef.current.files[0];
     const reader = new FileReader();
@@ -34,6 +37,7 @@ const Header = () => {
       });
 
       setOrdersData(ordersData);
+      setIsLoading(false); // Stop loading animation
       await router.push("/orders");
     };
 
@@ -46,7 +50,6 @@ const Header = () => {
         <h3 className={styles.headcolor}>GIPMER</h3>
         <h1 className={styles.heading}>UNLOCK THE POWER OF YOUR EXCEL DATA</h1>
         <p className={styles.text}>Transform Your Business Insights</p>
-        {/* Hidden file input */}
         <input
           ref={fileInputRef}
           type="file"
@@ -54,12 +57,13 @@ const Header = () => {
           style={{ display: "none" }}
           onChange={handleFileUpload}
         />
-        {/* Button triggers the hidden file input */}
+        {/* Button with conditional loading animation */}
         <button
           className={styles.button}
           onClick={() => fileInputRef.current.click()}
+          disabled={isLoading} // Disable button while loading
         >
-          UPLOAD DATA
+          {isLoading ? "Loading..." : "UPLOAD DATA"}
         </button>
       </div>
     </div>
